325f6838a116bbd39f3e52471c58a3d39eab7a1c
Quant-MaoTou <77672846+Quant-MaoTou@users.noreply.github.com>
2021-10-08 09:14:08 +0800

    Add files via upload

--- demo_strategies/demo_02_MACD_Numpy.py
+++ demo_strategies/demo_02_MACD_Numpy.py
@@ -5,6 +5,9 @@ MACD:(DIF-DEA)*2;
 
 金叉:=CROSS(DIF,DEA);
 死叉:=CROSS(DEA,DIF);
+
+买入:=金叉;
+卖出:=死叉;
 """
 
 import talib
@@ -12,6 +15,164 @@ import numpy as np
 import pandas as pd
 pd.set_option('expand_frame_repr', False)
 pd.set_option('display.max_rows', 20000)
+
+
+# 满仓
+def max_pos(price, cash, commission, min_size):
+	cash_available = cash * (1 - commission)
+	pos = cash_available / price // min_size * min_size
+	return pos
+
+
+# 当日无操作
+def no_trade(position, market_value, cash, total_asset, open_pct_change):
+	position[-1] = position[-2]  # P
+	cash[-1] = cash[-2]  # C
+	if position[-1] > 0:  # 持有多仓
+		market_value[-1] = market_value[-2] * (1 + open_pct_change[-1])  # M
+		total_asset[-1] = market_value[-1] + cash[-1]  # T
+	elif position[-1] < 0:  # 持有空仓
+		market_value[-1] = market_value[-2] * (1 - open_pct_change[-1])  # M
+		total_asset[-1] = market_value[-1] + cash[-1]  # T
+	elif position[-1] == 0:  # 若无持仓
+		market_value[-1] = 0  # M
+		total_asset[-1] = total_asset[-2]  # T
+
+
+# 无仓开多
+def empty_position_open_long(long_price,
+							 position, market_value, cash, total_asset,
+							 slippage, commission, min_size,
+							 volume=1, max_pos_in=False, target_pos_in=False, ):
+
+	# 滑点注入[^S]
+	buy_price = long_price[-1] + slippage
+
+	# 若设置百分比仓位
+	buy_position = volume
+	if max_pos_in:  # 满仓
+		buy_position = max_pos(price=buy_price,
+							   cash=cash[-2],
+							   commission=commission,
+							   min_size=min_size)
+	elif target_pos_in:  # 指定百分比仓位
+		buy_position = None
+		pass
+
+	# 开多
+	position[-1] = buy_position  # P
+	buy_value = buy_position * buy_price  # V
+	market_value[-1] = buy_value  # M
+	cash[-1] = cash[-2] - buy_value  # C
+	cash[-1] -= buy_value * commission  # [^C]
+	total_asset[-1] = market_value[-1] + cash[-1]  # T
+
+
+# 平空开多 后续持有多仓
+def close_short_open_long(short_price, long_price,
+						  position, market_value, cash, total_asset,
+						  slippage, commission, min_size,
+						  volume=1, max_pos_in=False, target_pos_in=False, ):
+
+	# 滑点注入 [^S]
+	sell_price = short_price[-1] - slippage
+	buy_price = long_price[-1] + slippage
+
+	# 平空
+	sell_value = sell_price * abs(position[-2])  # V
+	position[-1] = 0  # P
+	market_value[-1] = 0  # M
+	cash[-1] = cash[-2] + sell_value  # C
+	cash[-1] -= sell_value * commission  # [^C]
+	total_asset[-1] = cash[-1]  # T
+
+	# 开多
+	# 若设置百分比仓位
+	buy_position = volume
+	if max_pos_in:  # 满仓
+		buy_position = max_pos(price=buy_price,
+							   cash=cash[-1],
+							   commission=commission,
+							   min_size=min_size)
+	elif target_pos_in:  # 指定百分比仓位
+		buy_position = None
+		pass
+
+	position[-1] = buy_position  # P
+	buy_value = buy_position * buy_price  # V
+	market_value[-1] = buy_value  # M
+	cash[-1] = cash[-1] - buy_value  # C
+	cash[-1] -= buy_value * commission  # [^C]
+	total_asset[-1] = market_value[-1] + cash[-1]  # T
+
+
+# 无仓开空
+def empty_position_open_short(short_price,
+							  position, market_value, cash, total_asset,
+							  slippage, commission, min_size,
+							  volume=1, max_pos_in=False, target_pos_in=False, ):
+	# 滑点注入 [^S]
+	sell_price = short_price[-1] - slippage
+
+	# 开空
+	# 若设置百分比仓位
+	sell_position = volume
+	if max_pos_in:  # 满仓
+		sell_position = max_pos(price=sell_price,
+								cash=cash[-2],
+								commission=commission,
+								min_size=min_size)
+		sell_position = - sell_price  # P --
+	elif target_pos_in:  # 指定百分比仓位
+		sell_position = 999999999999999999
+		sell_position = - sell_position  # P --
+		pass
+
+	sell_value = abs(sell_position) * sell_price  # V ++
+	position[-1] = sell_position  # P --
+	market_value[-1] = sell_value  # M ++
+	cash[-1] = cash[-2] - sell_value  # C
+	cash[-1] -= sell_value * commission  # [^C]
+	total_asset[-1] = market_value[-1] + cash[-1]  # T
+
+
+# 平多开空 后续持有空仓
+def close_long_open_short(long_price, short_price,
+						  position, market_value, cash, total_asset,
+						  slippage, commission, min_size,
+						  volume=1, max_pos_in=False, target_pos_in=False, ):
+	# 滑点注入 [^S]
+	buy_price = long_price[-1] + slippage
+	sell_price = short_price[-1] - slippage
+
+	# 平多
+	sell_value = buy_price * position[-2]
+	position[-1] = 0  # P
+	market_value[-1] = 0  # M
+	cash[-1] = cash[-2] + sell_value  # C
+	cash[-1] -= sell_value * commission  # [^C]
+	total_asset[-1] = cash[-1]  # T
+
+	# 开空
+	# 若设置百分比仓位
+	sell_position = volume
+	if max_pos_in:  # 满仓
+		sell_position = max_pos(price=buy_price,
+								cash=cash[-1],
+								commission=commission,
+								min_size=min_size)
+		sell_position = - sell_position  # P --
+	elif target_pos_in:  # 指定百分比仓位
+		sell_position = 999999999999
+		sell_position = - sell_position  # P --
+		pass
+
+	position[-1] = sell_position  # P --
+	sell_value = abs(sell_position) * sell_price  # V ++
+	market_value[-1] = sell_value  # M
+	cash[-1] = cash[-1] - sell_value  # C
+	cash[-1] -= sell_value * commission  # [^C]
+	total_asset[-1] = market_value[-1] + cash[-1]  # T
 
 
 # 导入数据
@@ -53,18 +214,12 @@ init_min_size = 100
 init_slippage = 0.01
 
 
-# 删除无用列 添加 position market_value cash total_asset [P-M-C-T] 列
+# 删除无用列 添加 position market_value cash total_asset [P-M-C-T ^C^S] 列
 df = df[['code', 'open', 'open_pct_change', 'trade']]
 df.loc[df.index[0], 'position'] = 0
 df.loc[df.index[0], 'market_value'] = 0
 df.loc[df.index[0], 'total_asset'] = init_cash
 df.loc[df.index[0], 'cash'] = init_cash
-
-
-def max_pos(price, cash, commission, min_size):
-	cash_available = cash * (1 - commission)
-	pos = cash_available / price // min_size * min_size
-	return pos
 
 
 # 根据 [T+1日开盘] 的交易信号 进行交易
@@ -84,73 +239,45 @@ for i in range(1, len(df)):
 	# ==== -1 表示 T 日当前 bar, -2 表示 T-1日 past_bar ====
 	# ==== market_value 默认为 abs() 值 即空仓也视为正数的市值 ====
 	if trade[-1] == 0:  # 当日无交易
-		position[-1] = position[-2]  # P
-		cash[-1] = cash[-2]  # C
-		if position[-1] > 0:  # 持有多仓
-			market_value[-1] = market_value[-2] * (1 + open_pct_change[-1])  # M
-			total_asset[-1] = market_value[-1] + cash[-1]  # T
-		elif position[-1] < 0:  # 持有空仓
-			market_value[-1] = market_value[-2] * (1 - open_pct_change[-1])  # M
-			total_asset[-1] = market_value[-1] + cash[-1]  # T
-		elif position[-1] == 0:  # 若无持仓
-			market_value[-1] = 0  # M
-			total_asset[-1] = total_asset[-2]  # T
+		no_trade(position=position, market_value=market_value,
+			     cash=cash, total_asset=total_asset,
+			     open_pct_change=open_pct_change)
 
 	elif trade[-1] == 1:  # 当日有交易 买入做多
-		if position[-2] == 0:  # first-start
-			buy_price = open[-1] + init_slippage  # 滑点注入
-			buy_position = max_pos(price=buy_price,
-								   cash=cash[-2],
-								   commission=init_commission,
-								   min_size=init_min_size)  # P
-			position[-1] = buy_position  # P
-			buy_value = buy_position * buy_price  # Buy-Value !== Market-Value 考虑加仓情况
-			market_value[-1] = buy_value  # M
-			cash[-1] = cash[-2] - buy_value  # C
-			cash[-1] -= buy_value * (2 / 1000)  # C
-			total_asset[-1] = market_value[-1] + cash[-1]  # T
+		if position[-2] == 0:  # first-start 无仓 初始买入 无仓开多
+			empty_position_open_long(long_price=open, position=position,
+									 market_value=market_value, cash=cash, total_asset=total_asset,
+									 slippage=init_slippage, commission=init_commission, min_size=init_min_size,
+									 max_pos_in=True)
 
-		elif position[-2] < 0:  # already-start
-			cash[-1] = cash[-2] + market_value[-2]  # 平空 入现金 C
-			buy_price = open[-1] + init_slippage  # 滑点注入
-			buy_position = max_pos(price=open[-1],
-								   cash=cash[-1],
-								   commission=init_commission,
-								   min_size=init_min_size)
-			position[-1] = buy_position  # P
-			buy_value = buy_position * buy_price
-			market_value[-1] = buy_value
-			cash[-1] = cash[-1] - buy_value  # C
-			cash[-1] -= buy_value * (2 / 1000)  # C
-			total_asset[-1] = market_value[-1] + cash[-1]  # T
+		elif position[-2] < 0:  # already-start 已经持有空仓 平空开多
+			close_short_open_long(short_price=open, long_price=open,
+								  position=position, market_value=market_value, cash=cash, total_asset=total_asset,
+								  slippage=init_slippage, commission=init_commission, min_size=init_min_size,
+								  max_pos_in=True)
+
+		elif position[-2] > 0:  # already-start 已经持有多仓 即相邻的多头信号重复出现 此策略执行 [当日无交易] 代码段
+			no_trade(position=position, market_value=market_value,
+					 cash=cash, total_asset=total_asset,
+					 open_pct_change=open_pct_change)
 
 	elif trade[-1] == -1:  # 当日有交易 卖出做空
-		if position[-2] == 0:  # first-start
-			sell_price = open[-1] - init_slippage  # 滑点注入
-			sell_position = - max_pos(price=open[-1],
-									  cash=cash[-2],  # C
-									  commission=init_commission,
-									  min_size=init_min_size)  # --
-			sell_value = abs(sell_position) * sell_price  # ++
-			position[-1] = sell_position  # P --
-			market_value[-1] = sell_value  # M ++
-			cash[-1] = cash[-2] - sell_value  # C
-			cash[-1] -= sell_value * (2 / 1000)
-			total_asset[-1] = market_value[-1] + cash[-1]  # T
+		if position[-2] == 0:  # first-start 无仓 初始卖空 无仓开空
+			empty_position_open_short(short_price=open,
+									  position=position, market_value=market_value, cash=cash, total_asset=total_asset,
+									  slippage=init_slippage, commission=init_commission, min_size=init_min_size,
+									  max_pos_in=True)
 
-		elif position[-2] > 0:  # already-start
-			sell_price = open[-1] - init_slippage  # 滑点注入
-			cash[-1] = cash[-2] + market_value[-2]  # 平多 入现金 C
-			sell_position = - max_pos(price=sell_price,
-									  cash=cash[-1],  # C
-									  commission=init_commission,
-									  min_size=init_min_size)  # --
-			sell_value = abs(sell_position) * sell_price  # --
-			position[-1] = sell_position  # P --
-			market_value[-1] = sell_value  # M ++
-			cash[-1] = cash[-1] - sell_value  # C
-			cash[-1] -= sell_value * (2 / 1000)
-			total_asset[-1] = market_value[-1] + cash[-1]  # T
+		elif position[-2] > 0:  # already-start 已经持有多仓 平多开空
+			close_long_open_short(long_price=open, short_price=open,
+								  position=position, market_value=market_value, cash=cash, total_asset=total_asset,
+								  slippage=init_slippage, commission=init_commission, min_size=init_min_size,
+								  max_pos_in=True)
+
+		elif position[-2] < 0:  # already-start 已经持有空仓 即相邻的空头信号重复出现 此此略执行 [当日无交易] 代码段
+			no_trade(position=position, market_value=market_value,
+					 cash=cash, total_asset=total_asset,
+					 open_pct_change=open_pct_change)
 
 	df.loc[index, 'trade'] = trade[-1]
 	df.loc[index, 'position'] = position[-1]
@@ -164,6 +291,7 @@ for i in range(1, len(df)):
 # 打印 dataframe 末尾部分
 final_total_asset = df.loc[df.index[-1], 'total_asset']
 print('Final Total Asset = {} RMB'.format(final_total_asset))
+# exit()
 
 # 策略绩效分析 生成策略报告
 import quantstats, webbrowser
